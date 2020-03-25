5f70ba81b9658a2bcd9e01701eaba02dbcd280ea
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2020-03-25 09:35:02 +0100

    sdfghjk

--- CA200325A/Futo.cs
+++ CA200325A/Futo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,13 +14,8 @@ namespace CA200325A
         public bool Kategoria { get; set; }
         public TimeSpan Versenyido { get; set; }
         public int Szazalek { get; set; }
-
-        public string VersenyidoString =>
-
-            string.Format("{0:00}:{1:00}:{2:00}",
-                Versenyido.Days * 24 + Versenyido.Hours,
-                Versenyido.Minutes,
-                Versenyido.Seconds);
+        public string VersenyidoString { get; }
+        public double IdoOraban => Versenyido.TotalHours;
 
         public Futo(string s)
         {
@@ -28,10 +24,9 @@ namespace CA200325A
             Nev = t[0];
             Rajtszam = int.Parse(t[1]);
             Kategoria = t[2] == "Ferfi";
-
             var ido = t[3].Split(':');
-
             Versenyido = new TimeSpan(int.Parse(ido[0]), int.Parse(ido[1]), int.Parse(ido[2]));
+            VersenyidoString = t[3];
             Szazalek = int.Parse(t[4]);
 
         }
--- CA200325A/Program.cs
+++ CA200325A/Program.cs
@@ -16,7 +16,51 @@ namespace CA200325A
             F03();
             //FTeszt();
             F04();
+            F05();
+            F07();
+            F08("no");
+            F08("ferfi");
+            
             Console.ReadKey();
+        }
+
+        private static void F08(string sex)
+        {
+
+            Futo r = versenyzok
+                .Where(f => f.Kategoria == (sex == "ferfi") && f.Szazalek == 100)
+                .OrderBy(f => f.Versenyido)
+                .First();
+
+            Console.WriteLine($"f08: {sex}: {r.Nev} ({r.Rajtszam}.) - {r.VersenyidoString}");
+        }
+
+        private static void F07()
+        {
+            double r = versenyzok
+                .Where(f => f.Kategoria && f.Szazalek == 100)
+                .Average(f => f.IdoOraban);
+
+            Console.WriteLine($"f07: {r}");
+        }
+
+        private static void F05()
+        {
+            Console.Write("f05 input: ");
+            string nev = Console.ReadLine();
+            
+            int r = versenyzok
+                .Where(f => f.Nev == nev)
+                .Select(f => f.Szazalek)
+                .FirstOrDefault();
+
+            if(r != 0)
+            {
+                Console.WriteLine($"f05: {nev} indult a versenyen\n" +
+                    $"{(r == 100 ? "\t" : "\tnem ")}teljesítette a teljes távolt");
+            }
+            else Console.WriteLine($"f05: nem indult {nev} nevű");
+
         }
 
         private static void F04()
