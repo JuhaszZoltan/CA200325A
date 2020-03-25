fb60de64d0fa29d5ea85a2ad6fc8b09fc2e922c9
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2020-03-25 08:42:29 +0100

    Add project files.

---
+++ CA200325A/Futo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA200325A
+{
+    class Futo
+    {
+        public string Nev { get; set; }
+        public int Rajtszam { get; set; }
+        public bool Kategoria { get; set; }
+        public TimeSpan Versenyido { get; set; }
+        public int Szazalek { get; set; }
+
+        public string VersenyidoString =>
+
+            string.Format("{0:00}:{1:00}:{2:00}",
+                Versenyido.Days * 24 + Versenyido.Hours,
+                Versenyido.Minutes,
+                Versenyido.Seconds);
+
+        public Futo(string s)
+        {
+            var t = s.Split(';');
+
+            Nev = t[0];
+            Rajtszam = int.Parse(t[1]);
+            Kategoria = t[2] == "Ferfi";
+
+            var ido = t[3].Split(':');
+
+            Versenyido = new TimeSpan(int.Parse(ido[0]), int.Parse(ido[1]), int.Parse(ido[2]));
+            Szazalek = int.Parse(t[4]);
+
+        }
+    }
+}
---
+++ CA200325A/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA200325A
+{
+    class Program
+    {
+        static List<Futo> versenyzok;
+        static void Main()
+        {
+            Beolvas();
+            F03();
+            //FTeszt();
+            F04();
+            Console.ReadKey();
+        }
+
+        private static void F04()
+        {
+            var c = versenyzok.Count(f => !f.Kategoria && f.Szazalek == 100);
+            Console.WriteLine($"f04: {c}");
+        }
+
+        private static void FTeszt()
+        {
+            foreach (var f in versenyzok)
+            {
+                Console.WriteLine(f.VersenyidoString);
+            }
+        }
+        private static void F03()
+        {
+            Console.WriteLine($"f03: {versenyzok.Count}");
+        }
+        private static void Beolvas()
+        {
+            versenyzok = new List<Futo>();
+            var sr = new StreamReader(@"..\..\Res\ub2017egyeni.txt", Encoding.UTF8);
+
+            sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                versenyzok.Add(new Futo(sr.ReadLine()));
+            }
+            sr.Close();
+        }
+    }
+}
